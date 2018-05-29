4ec1aa220d18237b73d4d587f6be466f5fca822b
Moe <4352580+msafadieh@users.noreply.github.com>
2018-05-29 19:32:37 -0400

    used a better implementation of monthDays

--- ramadanTimes.py
+++ ramadanTimes.py
@@ -3,7 +3,6 @@ import requests, json, time, calendar, sys
 month = int(time.strftime('%m'))
 year = int(time.strftime('%Y'))
 day = int(time.strftime('%d'))
-monthDays = [calendar.monthrange(year,i)[1] for i in range(1,13)]
 
 #### GETS YOUR LOCATION
 if len(sys.argv) == 1:
@@ -20,7 +19,7 @@ else:
     exit()
 
 #### finds fajr [today], fajr [tomorrow], and maghrib based on your location
-if day == monthDays[month-1]:
+if day == calendar.monthrange(year,month)[1]:
     fajrDay = 1
     fajrMonth = (month + 1) % 12
     if fajrMonth == 0:
