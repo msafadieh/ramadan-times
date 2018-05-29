05db667f8fbabdb20e422ab73beb184bfafaac23
Mohamad Safadieh <mosafadieh@gmail.com>
2018-05-29 19:58:49 -0400

    added a method variable to make it easier to change the calculation method

--- ramadanTimes.py
+++ ramadanTimes.py
@@ -4,6 +4,23 @@ month = int(time.strftime('%m'))
 year = int(time.strftime('%Y'))
 day = int(time.strftime('%d'))
 
+# METHODS:
+# 0 - Shia Ithna-Ansari
+# 1 - University of Islamic Sciences, Karachi
+# 2 - Islamic Society of North America
+# 3 - Muslim World League
+# 4 - Umm Al-Qura University, Makkah
+# 5 - Egyptian General Authority of Survey
+# 7 - Institute of Geophysics, University of Tehran
+# 8 - Gulf Region
+# 9 - Kuwait
+# 10 - Qatar
+# 11 - Majlis Ugama Islam Singapura, Singapore
+# 12 - Union Organization islamic de France
+# 13 - Diyanet İşleri Başkanlığı, Turkey
+# 99 - Custom. See https://aladhan.com/calculation-methods
+method = 2
+
 #### GETS YOUR LOCATION
 if len(sys.argv) == 1:
     locationURL = 'http://api.ipstack.com/check?access_key=684b90ffe5e846163a1b244e3a1bab9c'
@@ -27,8 +44,8 @@ if day == calendar.monthrange(year,month)[1]:
     if month == 12:
         fajrYear = year + 1
 
-    url = 'http://api.aladhan.com/v1/calendarByAddress?address=%s&method=6&month=%s&year=%s' % (location, month, year)
-    fajrUrl = 'http://api.aladhan.com/v1/calendarByAddress?address=%s&method=2&month=%s&year=%s' % (location, fajrMonth, fajrYear)
+    url = 'http://api.aladhan.com/v1/calendarByAddress?address=%s&method=%d&month=%d&year=%d' % (location, method, month, year)
+    fajrUrl = 'http://api.aladhan.com/v1/calendarByAddress?address=%s&method=%d&month=%d&year=%d' % (location, method, fajrMonth, fajrYear)
 
     imported = requests.get(url)
     prayerData = json.loads(imported.text)['data']
@@ -39,7 +56,7 @@ if day == calendar.monthrange(year,month)[1]:
     print('Fajr [tomorrow]: ' + fajrPrayerData[fajrDay]['timings']['Fajr'])
 else:
     fajrDay = day+1
-    url = 'http://api.aladhan.com/v1/calendarByAddress?address=%s&method=2&month=%s&year=%s' % (location, month, year)
+    url = 'http://api.aladhan.com/v1/calendarByAddress?address=%s&method=%d&month=%d&year=%d' % (location, method, month, year)
     imported = requests.get(url)
     prayerData = json.loads(imported.text)['data']
     print('Fajr [today]: ' + prayerData[day]['timings']['Fajr'])
